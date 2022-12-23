423b54510017dfdb2eac108705f6603ecd786a5e
mayamaya99 <elmi33@hotmail.com>
2022-12-22 23:03:23 -0800

    add menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './allpages/navbar/navbar.component';
 import { FooterComponent } from './allpages/footer/footer.component';
 import { HomeComponent } from './pages/home/home.component';
+import { MenuComponent } from './pages/menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
